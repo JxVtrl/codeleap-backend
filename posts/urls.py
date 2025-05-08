44476a62eb2acc0af72ef7d801ee0f54888d7d4b
Joao Vitral <joaoviniciusvitral@hotmail.com>
2025-05-08 19:42:40 -0300

    Adicionar serializers e views para o modelo Post, além de configurar as URLs correspondentes

--- posts/urls.py
+++ posts/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import PostListCreate, PostUpdateDelete
 
-urlpatterns = []
+urlpatterns = [
+    path('', PostListCreate.as_view(), name='post-list-create'),
+    path('<int:pk>/', PostUpdateDelete.as_view(), name='post-update-delete'),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListCreate(generics.ListCreateAPIView):
+    queryset = Post.objects.all().order_by('-created_datetime')
+    serializer_class = PostSerializer
+
+class PostUpdateDelete(generics.UpdateAPIView, generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
